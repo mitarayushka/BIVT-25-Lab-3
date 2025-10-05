755ba91bf8faa6727af4a5a8a1ba5fa72dd34cf0
mitarayushka <126961692+mitarayushka@users.noreply.github.com>
2025-10-05 20:37:11 +0300

    Update Blue.cs

--- Lab3/Blue.cs
+++ Lab3/Blue.cs
@@ -1,13 +1,21 @@
-﻿namespace Lab3
+namespace Lab3
 {
     public class Blue
     {
         public double Task1(int n, int glass, int norma)
         {
-            double milk = 0;
+            double milk= 0;
 
             // code here
-
+            for (int i = 0; i < n; i+=1)
+            {
+                double weight = Convert.ToDouble(Console.ReadLine());
+                if (weight < norma)
+                {
+                    milk += glass;
+                }
+            }
+            milk = milk/ 1000;
             // end
 
             return milk;
@@ -17,7 +25,20 @@
             int first = 0, second = 0, third = 0, fourth = 0;
 
             // code here
-
+            double x, y;
+            for (int i = 0; i < n; i += 1)
+            {
+                x = double.Parse(Console.ReadLine());
+                y = double.Parse(Console.ReadLine());
+                if (x > 0 && y > 0)
+                    first += 1;
+                else if (x < 0 && y > 0)
+                    second += 1;
+                else if (x < 0 && y < 0)
+                    third += 1;
+                else if (x > 0 && y < 0)
+                    fourth+=1;
+            }
             // end
 
             return (first, second, third, fourth);
@@ -27,7 +48,18 @@
             int count = 0;
 
             // code here
-
+            for (int i = 0; i < n; i += 1)
+            {
+                int f = 1;
+                for (int j = 0; j < n; j += 1)
+                {
+                    int o = int.Parse(Console.ReadLine());
+                    if (o == 2 || o == 3)
+                        f = 0;
+                }
+                if (f == 1)
+                    count += 1;
+            }
             // end
 
             return count;
@@ -37,19 +69,67 @@
             int serias = 0;
 
             // code here
-
+            int serias_time, task_time = 10;
+            do
+            {
+                if (tasks > 0)
+                {
+                    time += task_time;
+                    task_time += 5;
+                    tasks--;
+                }
+                else
+                {
+                    serias_time = int.Parse(Console.ReadLine());
+                    time += serias_time;
+                    serias += 1;
+                }
+            }
+            while (time < 24 * 60);
             // end
 
             return (tasks, serias);
         }
-        public (int power, int agility, int intellect) Task5(int power, int agility, int intellect, int number)
+        public (int power, int agil, int intel) Task5(int power, int agil, int intel, int num)
         {
 
             // code here
-
+            switch (num)
+            {
+                case 1:
+                    power += 10;
+                    intel -= 5;
+                    break;
+                case 2:
+                    agil += 5;
+                    intel -= 5;
+                    power -= 5;
+                    break;
+                case 3:
+                    power += 10;
+                    intel -= 5;
+                    break;
+                case 4:
+                    agil += 15;
+                    power -= 10;
+                    intel -= 10;
+                    break;
+                case 5:
+                    intel += 7;
+                    power -= 5;
+                    break;
+                default:
+                    break;
+            }
+            if (power < 0)
+                power = 0;
+            if (agil < 0)
+                agil = 0;
+            if (intel < 0)
+                intel = 0;
             // end
 
-            return (power, agility, intellect);
+            return (power, agil, intel);
         }
     }
 }
